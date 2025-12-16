b18c1b135520f15289de5e2139a5141db12d187f
jasonli <lijason991@gmail.com>
2025-12-16 12:08:47 -0500

    fixed updateProduct to use PathVariable correctly

--- src/main/java/org/yearup/controllers/ProductsController.java
+++ src/main/java/org/yearup/controllers/ProductsController.java
@@ -71,7 +71,7 @@ public class ProductsController {
     public void updateProduct(@PathVariable int id, @RequestBody Product product) {
 
         try {
-            productDao.update(product.getProductId(), product);
+            productDao.update(id, product);
         }
         catch(Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
