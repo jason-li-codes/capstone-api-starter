58abaa7f0a85912f1951559dc6add7adff55aa16
lijas <lijason991@gmail.com>
2025-12-18 00:48:11 -0500

    completed required dao methods

--- src/main/java/org/yearup/data/mysql/MySqlUserDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlUserDao.java
@@ -12,23 +12,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MySqlUserDao extends MySqlDaoBase implements UserDao
-{
+public class MySqlUserDao extends MySqlDaoBase implements UserDao {
+
     @Autowired
-    public MySqlUserDao(DataSource dataSource)
-    {
+    public MySqlUserDao(DataSource dataSource) {
         super(dataSource);
     }
 
-
     @Override
-    public User create(User newUser)
-    {
-        String sql = "INSERT INTO users (username, hashed_password, role) VALUES (?, ?, ?)";
+    public User create(User newUser) {
+
+        String sql = """
+                INSERT INTO
+                    users (username, hashed_password, role)
+                VALUES
+                    (?, ?, ?)""";
+
         String hashedPassword = new BCryptPasswordEncoder().encode(newUser.getPassword());
 
-        try (Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
+
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, newUser.getUsername());
             ps.setString(2, hashedPassword);
@@ -36,123 +39,155 @@ public class MySqlUserDao extends MySqlDaoBase implements UserDao
 
             ps.executeUpdate();
 
-            User user = getByUserName(newUser.getUsername());
+            User user = getUserByUserName(newUser.getUsername());
             user.setPassword("");
 
             return user;
-
         }
-        catch (SQLException e)
-        {
+        catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public List<User> getAll()
-    {
+    public User update(User updatedUser) {
+
+        String sql = """
+                UPDATE
+                    users
+                SET
+                    username = ?,
+                    hashed_password = ?,
+                    role = ?
+                WHERE
+                    user_id = ?""";
+
+        String updatedHashedPassword = new BCryptPasswordEncoder().encode(updatedUser.getPassword());
+
+        try (Connection connection = getConnection()) {
+
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, updatedUser.getUsername());
+            ps.setString(2, updatedHashedPassword);
+            ps.setString(3, updatedUser.getRole());
+
+            ps.executeUpdate();
+
+            User user = getUserByUserName(updatedUser.getUsername());
+            user.setPassword("");
+
+            return user;
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+
         List<User> users = new ArrayList<>();
 
-        String sql = "SELECT * FROM users";
-        try (Connection connection = getConnection())
-        {
+        String sql = """
+                SELECT
+                    *
+                FROM
+                    users""";
+
+        try (Connection connection = getConnection()) {
+
             PreparedStatement statement = connection.prepareStatement(sql);
 
             ResultSet row = statement.executeQuery();
 
-            while (row.next())
-            {
+            while (row.next()) {
                 User user = mapRow(row);
                 users.add(user);
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
         return users;
     }
 
     @Override
-    public User getUserById(int id)
-    {
-        String sql = "SELECT * FROM users WHERE user_id = ?";
-        try (Connection connection = getConnection())
-        {
+    public User getUserById(int id) {
+
+        String sql = """
+                SELECT
+                    *
+                FROM
+                    users
+                WHERE
+                    user_id = ?""";
+
+        try (Connection connection = getConnection()) {
+
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, id);
 
             ResultSet row = statement.executeQuery();
 
-            if(row.next())
-            {
-                User user = mapRow(row);
-                return user;
+            if (row.next()) {
+                return mapRow(row);
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return null;
     }
 
     @Override
-    public User getByUserName(String username)
-    {
-        String sql = "SELECT * " +
-                " FROM users " +
-                " WHERE username = ?";
+    public User getUserByUserName(String username) {
 
-        try (Connection connection = getConnection())
-        {
+        String sql = """
+                SELECT
+                    *
+                FROM
+                    users
+                WHERE
+                    username = ?""";
+
+        try (Connection connection = getConnection()) {
+
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, username);
 
             ResultSet row = statement.executeQuery();
-            if(row.next())
-            {
-
-                User user = mapRow(row);
-                return user;
+            if (row.next()) {
+                return mapRow(row);
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        catch (SQLException e)
-        {
-            System.out.println(e);
-        }
-
         return null;
     }
 
     @Override
-    public int getIdByUsername(String username)
-    {
-        User user = getByUserName(username);
+    public int getIdByUsername(String username) {
 
-        if(user != null)
-        {
+        User user = getUserByUserName(username);
+
+        if(user != null) {
             return user.getId();
         }
-
         return -1;
     }
 
     @Override
-    public boolean exists(String username)
-    {
-        User user = getByUserName(username);
+    public boolean exists(String username) {
+
+        User user = getUserByUserName(username);
         return user != null;
     }
 
-    private User mapRow(ResultSet row) throws SQLException
-    {
+    private User mapRow(ResultSet row) throws SQLException {
+
         int userId = row.getInt("user_id");
         String username = row.getString("username");
         String hashedPassword = row.getString("hashed_password");
         String role = row.getString("role");
 
-        return new User(userId, username,hashedPassword, role);
+        return new User(userId, username, hashedPassword, role);
     }
 }
