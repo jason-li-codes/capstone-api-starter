af3e4d0c0e4a65e8931384f77b4186ed928f027d
lijas <lijason991@gmail.com>
2025-12-18 21:44:56 -0500

    completed required OrderController methods

---
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -0,0 +1,52 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.UserDao;
+import org.yearup.models.Order;
+import org.yearup.models.User;
+import org.yearup.services.OrderService;
+
+import java.security.Principal;
+
+@RestController
+@CrossOrigin
+@RequestMapping("orders")
+public class OrdersController {
+
+    private OrderService orderService;
+    private UserDao userDao;
+
+    @Autowired
+    public OrdersController(OrderService orderService, UserDao userDao) {
+        this.orderService = orderService;
+        this.userDao = userDao;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("isAuthenticated()")
+    public Order checkout(Principal principal) {
+
+        try {
+
+            User user = userDao.getUserByUserName(principal.getName());
+
+            if (user == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            int userId = user.getId();
+
+            Order order = orderService.checkout(userId);
+
+            if (order == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Checkout failed.");
+            return order;
+        } catch (ResponseStatusException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server not connected..");
+        }
+    }
+}
